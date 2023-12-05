8de322d9d1078a3833e2431fae9480f8b58c101b
Mony Dragon <monydragon@hotmail.com>
2023-12-04 21:26:13 -0600

    feat: Add theme preference functionality
    - Added a new import statement for the `MyMauiTemplate.Utilities` namespace.
    - Modified the `App` class constructor to set the `UserAppTheme` property using the `PreferencesHelper.GetTheme()` method.
    
    This commit adds functionality to allow users to set their preferred theme in the app. The `PreferencesHelper.GetTheme()` method retrieves the user's selected theme from storage, and it is then assigned to the `UserAppTheme` property in the constructor of the `App` class. This ensures that the app will display with the user's chosen theme upon launch.

--- MyMauiTemplate/App.xaml.cs
+++ MyMauiTemplate/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace MyMauiTemplate
+﻿using MyMauiTemplate.Utilities;
+
+namespace MyMauiTemplate
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
+            UserAppTheme = PreferencesHelper.GetTheme();
             MainPage = new AppShell();
         }
     }
